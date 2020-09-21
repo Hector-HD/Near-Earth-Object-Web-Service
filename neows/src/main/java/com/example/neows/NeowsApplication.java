9ac27e40cf95de51a61c164b6869314a3a45b7eb
Hector Duenas <hectorduenas.hd@gmail.com>
2020-09-20 19:53:05 -0700

    Verified that the connection with NASA API works

--- neows/src/main/java/com/example/neows/NeowsApplication.java
+++ neows/src/main/java/com/example/neows/NeowsApplication.java
@@ -2,12 +2,21 @@ package com.example.neows;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class NeowsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NeowsApplication.class, args);
+		getNeoWS();
 	}
 
+	public static void getNeoWS() {
+		String uri = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2020-09-07&end_date=2020-09-07&api_key=[API-KEY]";
+		
+		RestTemplate restTemplate = new RestTemplate();
+		String result = restTemplate.getForObject(uri, String.class);
+		System.out.println(result);
+	}
 }
