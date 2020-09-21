fb055c98ed07ad4cf49364fbd120d1d9ce26c0d3
Hector Duenas <hectorduenas.hd@gmail.com>
2020-09-20 19:46:02 -0700

    Added Spring Initializr

---
+++ neows/src/main/java/com/example/neows/NeowsApplication.java
@@ -0,0 +1,13 @@
+package com.example.neows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeowsApplication.class, args);
+	}
+
+}
---
+++ neows/src/test/java/com/example/neows/NeowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
